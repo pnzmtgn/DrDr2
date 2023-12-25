9efe5258b771de99024d4f80928c82e2af2d5924
pnzmtng <pnzmtgn@gmail.com>
2023-12-25 20:16:01 +0330

    view and urls updated to all specialties

--- Specialty/urls.py
+++ Specialty/urls.py
@@ -1,12 +1,8 @@
-from django.urls import path, include
-from .views import List, List_html
+from django.urls import path
+from .views import List_SPC, List_html
 
 urlpatterns = [
     path('', List_html),
-    path('Dermatology/', include('Dermatology.urls')),
-    path('Pulmonology/',include('Pulmonology.urls')),
-    path('Neurology/',include('Neurology.urls')),
-    path('Nephrology/',include('Nephrology.urls')),
-    path('Hematology/',include('Hematology.urls')),
-    path('Cardiology/',include('Cardiology.urls'))
+    path('<str:speciality_name/>', List_SPC)
 ]
+
